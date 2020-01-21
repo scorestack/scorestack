4a8f729de2ebd2d8185046a01769d42b4d998da9
Sean Newman <sxn6296@rit.edu>
2020-01-21 17:56:42 -0500

    Add generated dynamicbeat files

---
+++ beater/dynamicbeat.go
@@ -0,0 +1,71 @@
+package beater
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/elastic/beats/libbeat/beat"
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/logp"
+
+	"gitlab.ritsec.cloud/newman/dynamicbeat/config"
+)
+
+// Dynamicbeat configuration.
+type Dynamicbeat struct {
+	done   chan struct{}
+	config config.Config
+	client beat.Client
+}
+
+// New creates an instance of dynamicbeat.
+func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
+	c := config.DefaultConfig
+	if err := cfg.Unpack(&c); err != nil {
+		return nil, fmt.Errorf("Error reading config file: %v", err)
+	}
+
+	bt := &Dynamicbeat{
+		done:   make(chan struct{}),
+		config: c,
+	}
+	return bt, nil
+}
+
+// Run starts dynamicbeat.
+func (bt *Dynamicbeat) Run(b *beat.Beat) error {
+	logp.Info("dynamicbeat is running! Hit CTRL-C to stop it.")
+
+	var err error
+	bt.client, err = b.Publisher.Connect()
+	if err != nil {
+		return err
+	}
+
+	ticker := time.NewTicker(bt.config.Period)
+	counter := 1
+	for {
+		select {
+		case <-bt.done:
+			return nil
+		case <-ticker.C:
+		}
+
+		event := beat.Event{
+			Timestamp: time.Now(),
+			Fields: common.MapStr{
+				"type":    b.Info.Name,
+				"counter": counter,
+			},
+		}
+		bt.client.Publish(event)
+		logp.Info("Event sent")
+		counter++
+	}
+}
+
+// Stop stops dynamicbeat.
+func (bt *Dynamicbeat) Stop() {
+	bt.client.Close()
+	close(bt.done)
+}
