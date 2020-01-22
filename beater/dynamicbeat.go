8d49e16f6228bea5d4c6d1e61b10677e6feb00d5
Sean Newman <sxn6296@rit.edu>
2020-01-22 11:44:56 -0500

    Add esclient helper functions, refactor to use

--- beater/dynamicbeat.go
+++ beater/dynamicbeat.go
@@ -15,9 +15,9 @@ import (
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/go-elasticsearch"
-	"github.com/tidwall/gjson"
 
 	"gitlab.ritsec.cloud/newman/dynamicbeat/config"
+	"gitlab.ritsec.cloud/newman/dynamicbeat/esclient"
 )
 
 // Dynamicbeat configuration.
@@ -82,57 +82,31 @@ func (bt *Dynamicbeat) Run(b *beat.Beat) error {
 		}
 
 		// Get list of checks
-		resp, err := bt.es.Search(
-			bt.es.Search.WithIndex("checks"), // TODO: factor out checks index
-			bt.es.Search.WithStoredFields("_id"),
-		)
+		checks, err := esclient.GetAllDocuments(bt.es, "checks") // TODO: factor out checks index
 		if err != nil {
-			return fmt.Errorf("Error getting check IDs: %s", err)
+			return err
 		}
-		defer resp.Body.Close()
-		respBody := read(resp.Body)
-		checkIDs := gjson.Get(respBody, "hits.hits.#._id").Array()
 
 		// Iterate over each check
-		for _, checkID := range checkIDs {
-			// Get the check document
-			resp, err := bt.es.Get("checks", checkID.String())
-			if err != nil {
-				return fmt.Errorf("Error getting check %s: %s", checkID.String(), err)
-			}
-			defer resp.Body.Close()
-			document := gjson.Get(read(resp.Body), "_source").Map()
-
+		for _, check := range checks {
 			// Get any template variables for the check
-			attributes := make(map[string]string)
-			id := document["id"].String()
-			for _, indexType := range []string{"admin", "user"} {
+			attribs := make(map[string]string)
+			id := check["id"].String()
+			for _, perm := range []string{"admin", "user"} {
 				// Generate attribute index name
-				indexName := strings.Join([]string{"attrib_", indexType, "_", id}, "")
-
-				// Find index's ID
-				resp, err := bt.es.Search(
-					bt.es.Search.WithIndex(indexName),
-					bt.es.Search.WithStoredFields("_id"),
-				)
-				if err != nil {
-					return fmt.Errorf("Error getting attribute index %s: %s", indexName, err)
-				}
-				defer resp.Body.Close()
-				id := gjson.Get(read(resp.Body), "hits.hits.0._id").String()
+				idx := strings.Join([]string{"attrib_", perm, "_", id}, "")
 
 				// Get attribute document
-				resp, err = bt.es.Get(indexName, id)
+				docs, err := esclient.GetAllDocuments(bt.es, idx)
 				if err != nil {
-					return fmt.Errorf("Error reading attribute document %s: %s", id, err)
+					return err
 				}
-				defer resp.Body.Close()
-				attributeDocument := gjson.Get(read(resp.Body), "_source").Map()
+				attrib := docs[0]
 
 				// Read attributes from document
-				for key, val := range attributeDocument {
-					if _, present := attributes[key]; !present {
-						attributes[key] = val.String()
+				for k, v := range attrib {
+					if _, pres := attribs[k]; !pres {
+						attribs[k] = v.String()
 					}
 				}
 			}
@@ -141,18 +115,18 @@ func (bt *Dynamicbeat) Run(b *beat.Beat) error {
 			type NoopAttributes struct {
 				AdminName, TeamName string
 			}
-			templateAttributes := NoopAttributes{
-				AdminName: attributes["AdminName"],
-				TeamName:  attributes["TeamName"],
+			templAttribs := NoopAttributes{
+				AdminName: attribs["AdminName"],
+				TeamName:  attribs["TeamName"],
 			}
-			templatedDefinition := make(map[string]string)
-			for key, val := range document["definition"].Map() {
-				valTemplate := template.Must(template.New(key).Parse(val.String()))
+			def := make(map[string]string)
+			for k, v := range check["definition"].Map() {
+				templ := template.Must(template.New(k).Parse(v.String()))
 				var buf bytes.Buffer
-				if err := valTemplate.Execute(&buf, templateAttributes); err != nil {
-					return fmt.Errorf("Error parsing template for key %s: %s", key, err)
+				if err := templ.Execute(&buf, templAttribs); err != nil {
+					return fmt.Errorf("Error parsing template for key %s: %s", k, err)
 				}
-				templatedDefinition[key] = buf.String()
+				def[k] = buf.String()
 			}
 
 			// Send message
@@ -161,9 +135,9 @@ func (bt *Dynamicbeat) Run(b *beat.Beat) error {
 				Fields: common.MapStr{
 					"type": b.Info.Name,
 					"message": strings.Join([]string{
-						templatedDefinition["static"],
-						templatedDefinition["admin"],
-						templatedDefinition["team"],
+						def["static"],
+						def["admin"],
+						def["team"],
 					}, " - "),
 				},
 			}
