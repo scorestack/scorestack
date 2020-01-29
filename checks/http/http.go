fe690b96c7b349ad21aac94e97a326330a5aa1e0
Sean Newman <sxn6296@rit.edu>
2020-01-29 00:06:28 -0500

    Actually make HTTP GET requests

--- checks/http/http.go
+++ checks/http/http.go
@@ -1,6 +1,11 @@
 package http
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
+	"strings"
 	"time"
 
 	"gitlab.ritsec.cloud/newman/dynamicbeat/checks/common"
@@ -10,15 +15,61 @@ import (
 func Run(chk common.Check) {
 	defer chk.WaitGroup.Done()
 
+	startTime := time.Now()
+
+	// Set up result
 	result := common.CheckResult{
-		Timestamp: time.Now(),
+		Timestamp: startTime,
 		ID:        chk.ID,
 		Name:      chk.Name,
 		CheckType: "http",
-		Passed:    true,
-		Message:   "TODO",
+		Passed:    false,
+		Message:   "",
 		Details:   chk.Definition,
 	}
+
+	// Configure HTTP client
+	cookieJar, err := cookiejar.New(nil)
+	if err != nil {
+		result.Message = "Could not create CookieJar"
+		chk.Output <- result
+		return
+	}
+	client := &http.Client{
+		Jar: cookieJar,
+	}
+
+	// Construct URL
+	url := make([]string, 0)
+	if chk.Definition["https"] == "true" {
+		url = append(url, "https://")
+	} else {
+		url = append(url, "http://")
+	}
+	url = append(url, chk.Definition["host"])
+	if _, ok := chk.Definition["port"]; ok {
+		url = append(url, ":")
+		url = append(url, chk.Definition["port"])
+	}
+	url = append(url, chk.Definition["path"])
+	urlString := strings.Join(url, "")
+
+	details := make(map[string]string)
+	switch chk.Definition["method"] {
+	case "GET":
+		resp, err := client.Get(urlString)
+		if err != nil {
+			result.Message = fmt.Sprintf("Error with GET: %s", err)
+			chk.Output <- result
+			return
+		}
+		defer resp.Body.Close()
+		details["code"] = string(resp.StatusCode)
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		details["body"] = string(bodyBytes)
+	}
+
+	result.Details = details
 
 	chk.Output <- result
 }
