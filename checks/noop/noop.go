3d5e100bf60cdd70124fa739f08a2783e131247d
Sean Newman <sxn6296@rit.edu>
2020-02-05 10:18:55 -0500

    Reimplement noop check to use Check interface

--- checks/noop/noop.go
+++ checks/noop/noop.go
@@ -1,34 +1,50 @@
 package noop
 
 import (
+	"encoding/json"
 	"strings"
-	"time"
+	"sync"
 
 	"gitlab.ritsec.cloud/newman/dynamicbeat/checks/schema"
 )
 
-// Run : Execute the check
-func Run(chk schema.Check) {
-	defer chk.WaitGroup.Done()
+// The Definition configures the behavior of a Noop check.
+type Definition struct {
+	ID     string // a unique identifier for this check
+	Name   string // a human-readable title for this check
+	Admin  string // contains an attribute that only admins can modify
+	User   string // contains an attribute that users can modify
+	Static string // contains no attributes
+}
 
-	// Render the message string
-	message := make([]string, 0)
-	for k, v := range chk.Definition {
-		message = append(message, k)
-		message = append(message, ":")
-		message = append(message, v)
-		message = append(message, ",")
-	}
+// Run a single instance of the check.
+func (d Definition) Run(wg *sync.WaitGroup, out chan<- schema.CheckResult) {
+	defer wg.Done()
 
 	result := schema.CheckResult{
-		Timestamp: time.Now(),
-		ID:        chk.ID,
-		Name:      chk.Name,
+		Timestamp: time.now(),
+		ID:        d.ID,
+		Name:      d.Name,
 		CheckType: "noop",
 		Passed:    true,
-		Message:   strings.Join(message, " "),
-		Details:   chk.Definition,
+		Message:   strings.Join([]string{d.Admin, d.User, d.Static}),
+		Details: map[string]string{
+			"Admin":  d.Admin,
+			"User":   d.User,
+			"Static": d.Static,
+		},
 	}
 
-	chk.Output <- result
+	out <- result
+}
+
+// Init the check using a known ID and name. The rest of the check fields will
+// be filled in by parsing a JSON string representing the check definition.
+func (d Definition) Init(id string, name string, def []byte) error {
+	// Set ID and name attributes
+	d.ID = id
+	d.Name = name
+
+	// Unpack definition json
+	return json.Unmarshal(def, &d)
 }
