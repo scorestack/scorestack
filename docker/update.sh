295f6de43b8e22cdf95b0d32a3aa026a46af8c68
oneNutW0nder <scb5436@rit.edu>
2020-02-10 23:56:42 -0500

    Script for updating Elastic Indexes

---
+++ docker/update.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+for index in $(curl -sk -XGET -u root:changeme https://localhost:9200/_all | jq -r 'keys[]' | grep -E '(attrib|checks).*')
+do
+  curl -k -XDELETE -u root:changeme "https://localhost:9200/${index}"
+done
+
+# Set up example checks
+for check in $(find examples -maxdepth 1 -mindepth 1 -type d -printf "%f\n")
+do
+  # Add check definition
+  curl -k -XPOST -u root:changeme 'https://localhost:9200/checks/_doc' -H 'Content-Type: application/json' -d @examples/${check}/check.json
+
+  # Add admin attributes, if they are defined
+  if [ -f examples/${check}/admin-attribs.json ]
+  then
+    curl -k -XPOST -u root:changeme https://localhost:9200/attrib_admin_${check}-example/_doc -H "Content-Type: application/json" -d @examples/${check}/admin-attribs.json
+  fi
+
+  # Add user attributes, if they are defined
+  if [ -f examples/${check}/user-attribs.json ]
+  then
+    curl -k -XPOST -u root:changeme https://localhost:9200/attrib_user_${check}-example/_doc -H "Content-Type: application/json" -d @examples/${check}/user-attribs.json
+  fi
+done
+
+
