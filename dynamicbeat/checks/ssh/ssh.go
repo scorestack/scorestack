0fe2973abd69dd1a15b94070c8cee623d1bb12e7
oneNutW0nder <scb5436@rit.edu>
2020-02-10 22:50:17 -0500

    Added content matching and additional if-checks

--- dynamicbeat/checks/ssh/ssh.go
+++ dynamicbeat/checks/ssh/ssh.go
@@ -3,23 +3,27 @@ package ssh
 import (
 	"encoding/json"
 	"fmt"
+	"regexp"
 	"sync"
 	"time"
 
 	"github.com/s-newman/scorestack/dynamicbeat/checks/schema"
+	"golang.org/x/crypto/ssh"
 )
 
 // The Definition configures the behavior of the SSH check
 // it implements the "check" interface
 type Definition struct {
-	ID       string        // unique identifier for this check
-	Name     string        // a human-readable title for the check
-	IP       string        // (required) IP of the host to run the ICMP check against
-	Port     string        // (optional, default=22) The port to attempt an ssh connection on
-	Username string        // (required) The user to login with over ssh
-	Password string        // (required) The password for the user that you wish to login with
-	Cmd      string        // (required) The command to execute once ssh connection established
-	Timeout  time.Duration // (optional, default=5sec) How long to attempt a connection before terminating
+	ID           string // unique identifier for this check
+	Name         string // a human-readable title for the check
+	Group        string // (required) The group ID
+	IP           string // (required) IP of the host to run the ICMP check against
+	Username     string // (required) The user to login with over ssh
+	Password     string // (required) The password for the user that you wish to login with
+	Cmd          string // (required) The command to execute once ssh connection established
+	MatchContent bool   // (optional, default=false) Whether or not to match content like checking files
+	ContentRegex string // (optional, default=`.*`) Regex to match if reading a file
+	Port         string // (optional, default=22) The port to attempt an ssh connection on
 }
 
 // Run a single instance of the check
@@ -41,7 +45,7 @@ func (d *Definition) Run(wg *sync.WaitGroup, out chan<- schema.CheckResult) {
 			ssh.Password(d.Password),
 		},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-		Timeout:         d.Timeout,
+		Timeout:         5 * time.Second,
 	}
 
 	// Create the ssh client
@@ -62,15 +66,38 @@ func (d *Definition) Run(wg *sync.WaitGroup, out chan<- schema.CheckResult) {
 	defer session.Close()
 
 	// Run a command
-	output, err := session.CombinedOutput("/usr/bin/woami")
+	output, err := session.CombinedOutput(d.Cmd)
 	if err != nil {
 		result.Message = fmt.Sprintf("Error executing command: %s", err)
 		out <- result
 		return
 	}
 
-	// If we made it here the check passes
-	result.Message = fmt.Sprintf("Command %s executed successfully: %s", d.Cmd, output)
+	// Check if we are going to match content
+	if !d.MatchContent {
+		// If we made it here the check passes
+		result.Message = fmt.Sprintf("Command %s executed successfully: %s", d.Cmd, output)
+		result.Passed = true
+		out <- result
+		return
+	}
+
+	// Match some content
+	regex, err := regexp.Compile(d.ContentRegex)
+	if err != nil {
+		result.Message = fmt.Sprintf("Error compiling regex string %s : %s", d.ContentRegex, err)
+		out <- result
+		return
+	}
+
+	// Check if the content matches
+	if !regex.Match(output) {
+		result.Message = fmt.Sprintf("Matching content not found")
+		out <- result
+		return
+	}
+
+	// If we reach here the check is successful
 	result.Passed = true
 	out <- result
 }
@@ -94,9 +121,9 @@ func (d *Definition) Init(id string, name string, def []byte) error {
 		d.Port = "22"
 	}
 
-	// Check for optional timeout value
-	if d.Timeout == 0*time.Second {
-		d.Timeout = 5 * time.Second
+	// Check for optional ContentRegex
+	if d.ContentRegex == "" {
+		d.ContentRegex = ".*"
 	}
 
 	// Check for missing fields
