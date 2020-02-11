c359c1d80f3f0cf99685db625c9cf3b037623538
oneNutW0nder <scb5436@rit.edu>
2020-02-10 20:54:46 -0500

    Added port optional attrib

--- dynamicbeat/checks/ssh/ssh.go
+++ dynamicbeat/checks/ssh/ssh.go
@@ -2,6 +2,7 @@ package ssh
 
 import (
 	"encoding/json"
+	"fmt"
 	"sync"
 
 	"github.com/s-newman/scorestack/dynamicbeat/checks/schema"
@@ -13,13 +14,44 @@ type Definition struct {
 	ID       string // unique identifier for this check
 	Name     string // a human-readable title for the check
 	IP       string // (required) IP of the host to run the ICMP check against
+	Port     string // (optional, default=22) The port to attempt an ssh connection on
 	Username string // (required) The user to login with over ssh
 	Password string // (required) The password for the user that you wish to login with
 	Cmd      string // (required) The command to execute once ssh connection established
 }
 
 func (d *Definition) Run(wg *sync.WaitGroup, out chan<- schema.CheckResult) {
+	// Config SSH client
+	config := &ssh.ClientConfig{
+		User: d.Username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(d.Password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
 
+	// Create the ssh client
+	client, err := ssh.Dial("tcp", "localhost:22", config)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+
+	// Create a session from the connection
+	session, err := client.NewSession()
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
+	defer session.Close()
+
+	// Run a command
+	output, err := session.CombinedOutput("/usr/bin/woami")
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+
+	fmt.Printf("OUtput: %s\n", output)
 }
 
 // Init the check using a known ID and name. The rest of the check fields will
@@ -34,6 +66,11 @@ func (d *Definition) Init(id string, name string, def []byte) error {
 	err := json.Unmarshal(def, &d)
 	if err != nil {
 		return err
+	}
+
+	// Check for optional Port value
+	if d.Port != "" {
+		d.Port = "22"
 	}
 
 	// Check for missing fields
