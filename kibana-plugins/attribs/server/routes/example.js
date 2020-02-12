74dcc1b3e602e237a7e5cc3f368771f3e09c1d92
Sean Newman <sxn6296@rit.edu>
2020-02-11 23:37:34 -0500

    Remove example API route

--- kibana-plugins/attribs/server/routes/example.js
+++
@@ -1,9 +0,0 @@
-export default function(server) {
-  server.route({
-    path: '/api/attribs/example',
-    method: 'GET',
-    handler() {
-      return { time: new Date().toISOString() };
-    },
-  });
-}
