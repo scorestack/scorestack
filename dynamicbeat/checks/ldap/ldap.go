b559ad05634258d4101f70044cc2e863b2ab7fb6
oneNutW0nder <scb5436@rit.edu>
2020-02-12 23:18:45 -0500

    Set timeout for dialing connection

--- dynamicbeat/checks/ldap/ldap.go
+++ dynamicbeat/checks/ldap/ldap.go
@@ -36,6 +36,9 @@ func (d *Definition) Run(wg *sync.WaitGroup, out chan<- schema.CheckResult) {
 		CheckType: "ldap",
 	}
 
+	// Set timeout
+	ldap.DefaultTimeout = 5 * time.Second
+
 	// Normal, default ldap check
 	lconn, err := ldap.Dial("tcp", fmt.Sprintf("%s:%s", d.Fqdn, d.Port))
 	if err != nil {
