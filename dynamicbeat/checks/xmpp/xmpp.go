fd63b1b8da337634f2a574abcb343484c3d64173
oneNutW0nder <scb5436@rit.edu>
2020-02-15 20:27:21 -0500

    added more definition entries, started Run()

--- dynamicbeat/checks/xmpp/xmpp.go
+++ dynamicbeat/checks/xmpp/xmpp.go
@@ -2,30 +2,56 @@ package xmpp
 
 import (
 	"encoding/json"
+	"fmt"
 	"sync"
+	"time"
 
 	"github.com/s-newman/scorestack/dynamicbeat/checks/schema"
+	"gosrc.io/xmpp"
 )
 
 // The Definition configures the behavior of the XMPP check
 // it implements the "check" interface
 type Definition struct {
-	ID       string // a unique identifier for this check
-	Name     string // a human-readable title for the check
-	Group    string // the group this check is part of
-	Host     string // (required) IP or hostname of the xmpp server
-	Username string // (required) Username to use for the xmpp server
-	Password string // (required) Password for the user
+	ID        string // a unique identifier for this check
+	Name      string // a human-readable title for the check
+	Group     string // the group this check is part of
+	Host      string // (required) IP or hostname of the xmpp server
+	Username  string // (required) Username to use for the xmpp server
+	Password  string // (required) Password for the user
+	Encrypted bool   // (optional, default=true) TLS support or not
+	Port      string // (optional, default=5222) Port for the xmpp server
 }
 
 // Run a single instance of the check
 func (d *Definition) Run(wg *sync.WaitGroup, out chan<- schema.CheckResult) {
+	defer wg.Done()
+
+	// Set up result
+	result := schema.CheckResult{
+		Timestamp: time.Now(),
+		ID:        d.ID,
+		Name:      d.Name,
+		Group:     d.Group,
+		CheckType: "xmpp",
+	}
+
+	// Create xmpp config
+	config := xmpp.Config{
+		TransportConfiguration: xmpp.TransportConfiguration{
+			Address: fmt.Sprintf("%s:%s", d.Host, d.Port),
+		},
+	}
 
 }
 
 // Init the check using a known ID and name. The rest of the check fields will
 // be filled in by parsing a JSON string representing the check definition.
 func (d *Definition) Init(id string, name string, group string, def []byte) error {
+
+	// Set explicit values
+	d.Port = "5222"
+	d.Encrypted = true
 
 	// Unpack JSON definition
 	err := json.Unmarshal(def, &d)
