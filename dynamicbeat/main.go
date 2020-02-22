e4c8250077e4e89d16f8ab9841d0335067ca8e42
oneNutW0nder <scb5436@rit.edu>
2020-02-22 17:24:06 -0500

    added profiling

--- dynamicbeat/main.go
+++ dynamicbeat/main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"os"
 
+	"github.com/pkg/profile"
+
 	"github.com/s-newman/scorestack/dynamicbeat/cmd"
 
 	_ "github.com/s-newman/scorestack/dynamicbeat/include"
 )
 
+func run() error {
+	defer profile.Start(profile.GoroutineProfile).Stop()
+	return cmd.RootCmd.Execute()
+}
+
 func main() {
-	if err := cmd.RootCmd.Execute(); err != nil {
+	if err := run(); err != nil {
 		os.Exit(1)
 	}
 }
