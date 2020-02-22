d23d6113bbcb45efc9b397eeff61df0f2ebe95fe
oneNutW0nder <scb5436@rit.edu>
2020-02-22 16:40:45 -0500

    go back to less routine leaks

--- dynamicbeat/checks/vnc/vnc.go
+++ dynamicbeat/checks/vnc/vnc.go
@@ -54,11 +54,11 @@ func (d *Definition) Run(ctx context.Context) schema.CheckResult {
 		result.Message = fmt.Sprintf("Connection to VNC host %s failed : %s", d.Host, err)
 		return result
 	}
-	// defer func() {
-	// 	if closeErr := conn.Close(); closeErr != nil {
-	// 		logp.Warn("failed to close vnc connection: %s", closeErr.Error())
-	// 	}
-	// }()
+	defer func() {
+		if closeErr := conn.Close(); closeErr != nil {
+			logp.Warn("failed to close vnc connection: %s", closeErr.Error())
+		}
+	}()
 
 	vncClient, err := vnc.Client(conn, &config)
 	if err != nil {
