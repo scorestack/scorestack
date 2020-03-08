03a655ff7defa1748632cd3388f4cf36f992faf5
Sean Newman <sxn6296@rit.edu>
2020-03-08 14:37:12 -0400

    Update noop check

--- dynamicbeat/checks/noop/noop.go
+++ dynamicbeat/checks/noop/noop.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"strings"
-	"time"
 
 	"github.com/s-newman/scorestack/dynamicbeat/checks/schema"
 )
@@ -17,26 +16,19 @@ type Definition struct {
 }
 
 // Run a single instance of the check.
-func (d *Definition) Run(ctx context.Context, sendResult chan<- schema.CheckResult) {
+func (d *Definition) Run(ctx context.Context) schema.CheckResult {
+	// Initialize empty result
+	result := schema.CheckResult{}
 
-	result := schema.CheckResult{
-		Timestamp:   time.Now(),
-		ID:          d.Config.ID,
-		Name:        d.Config.Name,
-		Group:       d.Config.Group,
-		ScoreWeight: d.Config.ScoreWeight,
-		CheckType:   "noop",
-		Passed:      true,
-		Message:     strings.Join([]string{d.Dynamic, d.Static}, "; "),
-		Details: map[string]string{
-			"Dynamic": d.Dynamic,
-			"Static":  d.Static,
-		},
+	// "Run" the check
+	result.Passed = true
+	result.Message = strings.Join([]string{d.Dynamic, d.Static}, "; ")
+	result.Details = map[string]string{
+		"Dynamic": d.Dynamic,
+		"Static":  d.Static,
 	}
 
-	result.Passed = true
 	return result
-
 }
 
 // Init the check using a known ID and name. The rest of the check fields will
