d661c10ebe103c67082edc7bbbf6f5f1b34c6fa1
Sean Newman <sxn6296@rit.edu>
2020-04-03 01:33:23 -0400

    Create less indices in add-team script

--- add-team.sh
+++ add-team.sh
@@ -51,13 +51,13 @@ do
     # Add admin attributes, if they are defined
     if [ -f examples/${check}/admin-attribs.json ]
     then
-      curl -k -XPUT -u root:changeme https://${ELASTICSEARCH_HOST}/attrib_admin_${ID}/_doc/attributes -H "Content-Type: application/json" -d @examples/${check}/admin-attribs.json
+      curl -k -XPUT -u root:changeme https://${ELASTICSEARCH_HOST}/attrib_admin_${TEAM}/_doc/${ID} -H "Content-Type: application/json" -d @examples/${check}/admin-attribs.json
     fi
 
     # Add user attributes, if they are defined
     if [ -f examples/${check}/user-attribs.json ]
     then
-      curl -k -XPUT -u root:changeme https://${ELASTICSEARCH_HOST}/attrib_user_${ID}/_doc/attributes -H "Content-Type: application/json" -d @examples/${check}/user-attribs.json
+      curl -k -XPUT -u root:changeme https://${ELASTICSEARCH_HOST}/attrib_user_${TEAM}/_doc/${ID} -H "Content-Type: application/json" -d @examples/${check}/user-attribs.json
     fi
   done
 
