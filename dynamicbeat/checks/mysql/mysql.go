367b94d32cdfdf00d4c550e975728b4cc31ec396
Sean Newman <sxn6296@rit.edu>
2020-04-12 22:06:22 -0400

    Add MatchContent option

--- dynamicbeat/checks/mysql/mysql.go
+++ dynamicbeat/checks/mysql/mysql.go
@@ -21,6 +21,7 @@ type Definition struct {
 	Database     string             `optiontype:"required"`                      // Name of the database to access
 	Table        string             `optiontype:"required"`                      // Name of the table to access
 	Column       string             `optiontype:"required"`                      // Name of the column to access
+	MatchContent bool               `optiontype:"optional"`                      // Whether to perform a regex content match on the results of the query
 	ContentRegex string             `optiontype:"optional" optiondefault:".*"`   // Regex to match on
 	Port         string             `optiontype:"optional" optiondefault:"3306"` // Port for the server
 }
@@ -60,28 +61,31 @@ func (d *Definition) Run(ctx context.Context) schema.CheckResult {
 	// Store the value from the column
 	var val string
 
-	// Compile the regex
-	regex, err := regexp.Compile(d.ContentRegex)
-	if err != nil {
-		result.Message = fmt.Sprintf("Error compiling regex string %s : %s", d.ContentRegex, err)
-		return result
-	}
-
-	// Check the rows
-	for rows.Next() {
-		// Grab a value
-		err := rows.Scan(&val)
+	// Perform regex matching, if necessary
+	if d.MatchContent {
+		// Compile the regex
+		regex, err := regexp.Compile(d.ContentRegex)
 		if err != nil {
-			result.Message = fmt.Sprintf("Could not scan row values : %s", err)
-			return result
-		}
-		// Check value with regex
-		if regex.MatchString(val) {
-			// If we reach here the check passes
-			result.Passed = true
+			result.Message = fmt.Sprintf("Error compiling regex string %s : %s", d.ContentRegex, err)
 			return result
 		}
 
+		// Check the rows
+		for rows.Next() {
+			// Grab a value
+			err := rows.Scan(&val)
+			if err != nil {
+				result.Message = fmt.Sprintf("Could not scan row values : %s", err)
+				return result
+			}
+			// Check value with regex
+			if regex.MatchString(val) {
+				// If we reach here the check passes
+				result.Passed = true
+				return result
+			}
+
+		}
 	}
 
 	// Check for error in the rows
