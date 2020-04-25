85a01b1e30d9903b010c98f0d5a1041eac0c585c
Sean Newman <sxn6296@rit.edu>
2020-04-24 22:37:17 -0400

    Allow username and password to be set in add-team

--- add-team.sh
+++ add-team.sh
@@ -3,16 +3,18 @@
 ELASTICSEARCH_HOST=localhost:9200
 KIBANA_HOST=localhost:5601
 CHECK_FOLDER=examples
+USERNAME=root
+PASSWORD=changeme
 
 # Wait for elasticsearch to come up
-while [[ "$(curl -sku root:changeme "https://${ELASTICSEARCH_HOST}/_cluster/health" | jq -r .status 2>/dev/null)" != "green" ]]
+while [[ "$(curl -sku ${USERNAME}:${PASSWORD} "https://${ELASTICSEARCH_HOST}/_cluster/health" | jq -r .status 2>/dev/null)" != "green" ]]
 do
   echo "Waiting for Elasticsearch to be ready..."
   sleep 5
 done
 
 # Wait for kibana to come up
-while [[ "$(curl -sku root:changeme https://${KIBANA_HOST}/api/status | jq -r .status.overall.state 2>/dev/null)" != "green" ]]
+while [[ "$(curl -sku ${USERNAME}:${PASSWORD} https://${KIBANA_HOST}/api/status | jq -r .status.overall.state 2>/dev/null)" != "green" ]]
 do
   echo "Waiting for Kibana to be ready..."
   sleep 5
@@ -26,14 +28,14 @@ UUID_D=dddddddd-dddd-dddd-dddd-dddddddddddd
 UUID_E=eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee
 UUID_F=ffffffff-ffff-ffff-ffff-ffffffffffff
 cat dashboards/scoreboard.json | sed -e "s/\${UUID_A}/${UUID_A}/g" | sed -e "s/\${UUID_B}/${UUID_B}/g" | sed -e "s/\${UUID_C}/${UUID_C}/g" | sed -e "s/\${UUID_D}/${UUID_D}/g" | sed -e "s/\${UUID_E}/${UUID_E}/g" | sed -e "s/\${UUID_F}/${UUID_F}/g" > tmp-dashboard.json
-curl -ku root:changeme https://${KIBANA_HOST}/api/kibana/dashboards/import -H "Content-Type: application/json" -H "kbn-xsrf: true" -d @tmp-dashboard.json
-curl -kX POST -u root:changeme https://${KIBANA_HOST}/api/spaces/_copy_saved_objects -H 'Content-Type: application/json' -H 'kbn-xsrf: true' -d '{"spaces":["scorestack"],"objects":[{"type":"dashboard","id":"'${UUID_A}'"}],"includeReferences":true}'
+curl -ku ${USERNAME}:${PASSWORD} https://${KIBANA_HOST}/api/kibana/dashboards/import -H "Content-Type: application/json" -H "kbn-xsrf: true" -d @tmp-dashboard.json
+curl -kX POST -u ${USERNAME}:${PASSWORD} https://${KIBANA_HOST}/api/spaces/_copy_saved_objects -H 'Content-Type: application/json' -H 'kbn-xsrf: true' -d '{"spaces":["scorestack"],"objects":[{"type":"dashboard","id":"'${UUID_A}'"}],"includeReferences":true}'
 
 # Clean up
 rm tmp-dashboard.json
 
 # Add default index template
-curl -k -XPUT -u root:changeme https://${ELASTICSEARCH_HOST}/_template/default -H 'Content-Type: application/json' -d '{"index_patterns":["check*","attrib_*","results*"],"settings":{"number_of_replicas":"0"}}'
+curl -k -XPUT -u ${USERNAME}:${PASSWORD} https://${ELASTICSEARCH_HOST}/_template/default -H 'Content-Type: application/json' -d '{"index_patterns":["check*","attrib_*","results*"],"settings":{"number_of_replicas":"0"}}'
 
 # Loop through all teams passed as arguments
 for TEAM in "${@}"
@@ -45,28 +47,28 @@ do
     # Add check definition
     cat ${CHECK_FOLDER}/${check}/check.json | jq --arg TEAM "$TEAM" '.group = $TEAM | .id = "\(.id)-\($TEAM)"' | sed -e "s/\${TEAM_NUM}/${TEAM_NUM}/g" > check.tmp.json
     ID=$(cat check.tmp.json | jq -r '.id')
-    curl -k -XPUT -u root:changeme https://${ELASTICSEARCH_HOST}/checkdef/_doc/${ID} -H 'Content-Type: application/json' -d @check.tmp.json
+    curl -k -XPUT -u ${USERNAME}:${PASSWORD} https://${ELASTICSEARCH_HOST}/checkdef/_doc/${ID} -H 'Content-Type: application/json' -d @check.tmp.json
     cat check.tmp.json | jq '{id, name, type, group}' > generic-check.tmp.json
-    curl -k -XPUT -u root:changeme https://${ELASTICSEARCH_HOST}/checks/_doc/${ID} -H 'Content-Type: application/json' -d @generic-check.tmp.json
+    curl -k -XPUT -u ${USERNAME}:${PASSWORD} https://${ELASTICSEARCH_HOST}/checks/_doc/${ID} -H 'Content-Type: application/json' -d @generic-check.tmp.json
 
     # Add admin attributes, if they are defined
     if [ -f ${CHECK_FOLDER}/${check}/admin-attribs.json ]
     then
-      curl -k -XPUT -u root:changeme https://${ELASTICSEARCH_HOST}/attrib_admin_${TEAM}/_doc/${ID} -H "Content-Type: application/json" -d @${CHECK_FOLDER}/${check}/admin-attribs.json
+      curl -k -XPUT -u ${USERNAME}:${PASSWORD} https://${ELASTICSEARCH_HOST}/attrib_admin_${TEAM}/_doc/${ID} -H "Content-Type: application/json" -d @${CHECK_FOLDER}/${check}/admin-attribs.json
     fi
 
     # Add user attributes, if they are defined
     if [ -f ${CHECK_FOLDER}/${check}/user-attribs.json ]
     then
-      curl -k -XPUT -u root:changeme https://${ELASTICSEARCH_HOST}/attrib_user_${TEAM}/_doc/${ID} -H "Content-Type: application/json" -d @${CHECK_FOLDER}/${check}/user-attribs.json
+      curl -k -XPUT -u ${USERNAME}:${PASSWORD} https://${ELASTICSEARCH_HOST}/attrib_user_${TEAM}/_doc/${ID} -H "Content-Type: application/json" -d @${CHECK_FOLDER}/${check}/user-attribs.json
     fi
   done
 
   # Add team role
-  curl -kX PUT -u root:changeme https://${KIBANA_HOST}/api/security/role/${TEAM} -H 'Content-Type: application/json' -H 'kbn-xsrf: true' -d '{"elasticsearch":{"indices":[{"names":["results-'${TEAM}'*"],"privileges":["read"]},{"names":["attrib_user_*-'${TEAM}'"],"privileges":["read","index","view_index_metadata"]}]}}'
+  curl -kX PUT -u ${USERNAME}:${PASSWORD} https://${KIBANA_HOST}/api/security/role/${TEAM} -H 'Content-Type: application/json' -H 'kbn-xsrf: true' -d '{"elasticsearch":{"indices":[{"names":["results-'${TEAM}'*"],"privileges":["read"]},{"names":["attrib_user_*-'${TEAM}'"],"privileges":["read","index","view_index_metadata"]}]}}'
 
   # Add team user
-  curl -kX PUT -u root:changeme https://${ELASTICSEARCH_HOST}/_security/user/${TEAM} -H 'Content-Type: application/json' -d '{"password":"changeme","roles":["common","'${TEAM}'"]}'
+  curl -kX PUT -u ${USERNAME}:${PASSWORD} https://${ELASTICSEARCH_HOST}/_security/user/${TEAM} -H 'Content-Type: application/json' -d '{"password":"changeme","roles":["common","'${TEAM}'"]}'
 
   # Add team overview dashboard
   UUID_A=$(uuidgen)
@@ -78,8 +80,8 @@ do
   INDEX="results-${TEAM}*"
   CHECKS=$(find examples -maxdepth 1 -mindepth 1 -type d -printf "%f\n" | wc -l)
   cat dashboards/single-team-overview.json | sed -e "s/\${UUID_A}/${UUID_A}/g" | sed -e "s/\${UUID_B}/${UUID_B}/g" | sed -e "s/\${UUID_C}/${UUID_C}/g" | sed -e "s/\${UUID_D}/${UUID_D}/g" | sed -e "s/\${UUID_E}/${UUID_E}/g" | sed -e "s/\${UUID_F}/${UUID_F}/g" | sed -e "s/\${TEAM}/${TEAM}/g" | sed -e "s/\${INDEX}/${INDEX}/g" | sed -e "s/\${CHECKS}/${CHECKS}/g" > tmp-dashboard.json
-  curl -ku root:changeme https://${KIBANA_HOST}/api/kibana/dashboards/import -H "Content-Type: application/json" -H "kbn-xsrf: true" -d @tmp-dashboard.json
-  curl -kX POST -u root:changeme https://${KIBANA_HOST}/api/spaces/_copy_saved_objects -H 'Content-Type: application/json' -H 'kbn-xsrf: true' -d '{"spaces":["scorestack"],"objects":[{"type":"dashboard","id":"'${UUID_A}'"}],"includeReferences":true}'
+  curl -ku ${USERNAME}:${PASSWORD} https://${KIBANA_HOST}/api/kibana/dashboards/import -H "Content-Type: application/json" -H "kbn-xsrf: true" -d @tmp-dashboard.json
+  curl -kX POST -u ${USERNAME}:${PASSWORD} https://${KIBANA_HOST}/api/spaces/_copy_saved_objects -H 'Content-Type: application/json' -H 'kbn-xsrf: true' -d '{"spaces":["scorestack"],"objects":[{"type":"dashboard","id":"'${UUID_A}'"}],"includeReferences":true}'
 done
 
 # Clean up
