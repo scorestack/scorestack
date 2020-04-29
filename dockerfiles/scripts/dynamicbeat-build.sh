b69add39a68ed70aad8962e1ba205a84947bc61a
Sean Newman <sxn6296@rit.edu>
2020-04-29 02:34:35 -0400

    CI debugging 2

--- dockerfiles/scripts/dynamicbeat-build.sh
+++ dockerfiles/scripts/dynamicbeat-build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -euxo pipefail
 
-echo $GOPATH
 cd $GOPATH/src/github.com/s-newman/scorestack/dynamicbeat
+ls -l $GOPATH
+ls -l $GOPATH/src
+ls -l $GOPATH/src/github.com
+ls -l $GOPATH/src/github.com/s-newman
+ls -l $GOPATH/src/github.com/s-newman/scorestack
+ls -l $GOPATH/src/github.com/s-newman/scorestack/dynamicbeat
 go get
 make
