1053cb451e64c61ecaad9c2522aa9c5c21358df6
Sean Newman <sxn6296@rit.edu>
2020-04-29 03:44:31 -0400

    Ensure golangci-lint is in GOPATH

--- dockerfiles/scripts/dynamicbeat-lint.sh
+++ dockerfiles/scripts/dynamicbeat-lint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
+export PATH="$PATH:$GOPATH/bin"
 cd $GOPATH/src/github.com/s-newman/scorestack/dynamicbeat
 go get
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.25.1
