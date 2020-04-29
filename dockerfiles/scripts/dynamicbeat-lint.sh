ccd0b3e9871b7e352763bd5485f84b1b640024db
Sean Newman <sxn6296@rit.edu>
2020-04-29 03:34:25 -0400

    Install golangci-lint via binary

--- dockerfiles/scripts/dynamicbeat-lint.sh
+++ dockerfiles/scripts/dynamicbeat-lint.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 
 cd $GOPATH/src/github.com/s-newman/scorestack/dynamicbeat
 go get
-go get github.com/golangci/golangci-lint/cmd/golangci-lint
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.25.1
 golangci-lint run -v
