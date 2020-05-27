c75ce2ba45f3c0f8e194739c9f81b9cf4b496e6b
oneNutW0nder <scb5436@rit.edu>
2020-05-27 15:00:44 -0400

    Removed unecessary def item

--- dynamicbeat/checks/ftp/ftp.go
+++ dynamicbeat/checks/ftp/ftp.go
@@ -16,17 +16,16 @@ import (
 // The Definition configures the behavior of the FTP check
 // it implements the "check" interface
 type Definition struct {
-	Config            schema.CheckConfig // generic metadata about the check
-	Host              string             `optiontype:"required"`                      // IP or hostname of the host to run the FTP check against
-	Username          string             `optiontype:"required"`                      // The user to login with over FTP
-	Password          string             `optiontype:"required"`                      // The password for the user that you wish to login with
-	File              string             `optiontype:"required"`                      // The path to the file to access during the FTP check
-	RegexContentMatch bool               `optiontype:"optional" optiondefault:"true"` // Whether or not to match file content with regex
-	ContentRegex      string             `optiontype:"optional" optiondefault:".*"`   // Regex to match if reading a file
-	HashContentMatch  bool               `optiontype:"optional"`                      // Whether or not to match a hash of the file contents
-	Hash              string             `optiontype:"optional"`                      // The hash digest from sha3-256 to compare the hashed file contents to
-	Port              string             `optiontype:"optional" optiondefault:"21"`   // The port to attempt an ftp connection on
-	Fucked            bool               `optiontype:"optional"`                      // Custom case for Cerealkiller ISTS2020
+	Config           schema.CheckConfig // generic metadata about the check
+	Host             string             `optiontype:"required"`                    // IP or hostname of the host to run the FTP check against
+	Username         string             `optiontype:"required"`                    // The user to login with over FTP
+	Password         string             `optiontype:"required"`                    // The password for the user that you wish to login with
+	File             string             `optiontype:"required"`                    // The path to the file to access during the FTP check
+	ContentRegex     string             `optiontype:"optional" optiondefault:".*"` // Regex to match if reading a file
+	HashContentMatch bool               `optiontype:"optional"`                    // Whether or not to match a hash of the file contents
+	Hash             string             `optiontype:"optional"`                    // The hash digest from sha3-256 to compare the hashed file contents to
+	Port             string             `optiontype:"optional" optiondefault:"21"` // The port to attempt an ftp connection on
+	Fucked           bool               `optiontype:"optional"`                    // Custom case for Cerealkiller ISTS2020
 }
 
 // Run a single instance of the check
