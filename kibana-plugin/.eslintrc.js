eee0414d6c8a2406b7ff1cc541ed6dd6a264f25f
Sean Newman <sxn6296@rit.edu>
2020-06-15 20:59:35 -0400

    Update plugin folder root-level configs

--- kibana-plugin/.eslintrc.js
+++ kibana-plugin/.eslintrc.js
@@ -1,24 +1,4 @@
 module.exports = {
   root: true,
   extends: ['@elastic/eslint-config-kibana', 'plugin:@elastic/eui/recommended'],
-  settings: {
-    'import/resolver': {
-      '@kbn/eslint-import-resolver-kibana': {
-        rootPackageName: 'kibana-plugin',
-      }
-    },
-  },
-  overrides: [
-    {
-      files: ['**/public/**/*'],
-      settings: {
-        'import/resolver': {
-          '@kbn/eslint-import-resolver-kibana': {
-            forceNode: false,
-            rootPackageName: 'kibana-plugin',
-          },
-        },
-      },
-    },
-  ]
 };
