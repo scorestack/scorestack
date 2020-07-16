09e784ab05d13808ef6ca2847acd27b66165ab7c
Sean Newman <sxn6296@rit.edu>
2020-07-15 23:39:43 -0400

    Render minimal template form

--- kibana-plugin/public/components/templates/template.tsx
+++ kibana-plugin/public/components/templates/template.tsx
@@ -1,16 +1,21 @@
 import {
   EuiButtonEmpty,
+  EuiFieldText,
   EuiFlexGroup,
   EuiFlexItem,
+  EuiForm,
+  EuiFormRow,
   EuiPageContent,
+  EuiPageContentBody,
   EuiPageContentHeader,
-  EuiPageContentHeaderSection,
   EuiPageHeader,
+  EuiSelect,
+  EuiTextArea,
   EuiTitle,
 } from '@elastic/eui';
 import React, { Fragment, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Protocol } from '../../../common/checks';
+import { Protocol, ProtocolList } from '../../../common/checks';
 import { ITemplate } from '../../../common/types';
 
 interface TemplateProps {
@@ -53,12 +58,39 @@ export function Template(props: TemplateProps): React.ReactElement {
       </EuiPageHeader>
       <EuiPageContent>
         <EuiPageContentHeader>
-          <EuiPageContentHeaderSection>
-            <EuiTitle>
-              <h1>{template.title}</h1>
-            </EuiTitle>
-          </EuiPageContentHeaderSection>
+          <EuiTitle>
+            <h1>Template Editor</h1>
+          </EuiTitle>
         </EuiPageContentHeader>
+        <EuiPageContentBody>
+          <EuiForm>
+            <EuiFormRow label="Title">
+              <EuiFieldText
+                name="title"
+                value={template.title}
+                onChange={(event) => setTemplate({ title: event.target.value, ...template })}
+              />
+            </EuiFormRow>
+
+            <EuiFormRow label="Description">
+              <EuiTextArea
+                name="description"
+                value={template.description}
+                onChange={(event) => setTemplate({ description: event.target.value, ...template })}
+              />
+            </EuiFormRow>
+
+            <EuiFormRow label="Protocol">
+              <EuiSelect
+                options={ProtocolList.map((proto) => {
+                  return { value: proto, text: proto };
+                })}
+                value={template.protocol}
+                onChange={(event) => setTemplate({ protocol: event.target.value, ...template })}
+              />
+            </EuiFormRow>
+          </EuiForm>
+        </EuiPageContentBody>
       </EuiPageContent>
     </Fragment>
   );
