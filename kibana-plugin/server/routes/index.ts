557f039d7b75964c40557396e13fa53a36cbbef7
Sean Newman <sxn6296@rit.edu>
2020-07-24 00:19:05 -0400

    Add another GET ./template route to get all

--- kibana-plugin/server/routes/index.ts
+++ kibana-plugin/server/routes/index.ts
@@ -6,6 +6,7 @@ import {
   SavedObjectsClient,
   SavedObject,
   SavedObjectsFindResponse,
+  SavedObjectsFindOptions,
 } from '../../../../src/core/server';
 
 import { PLUGIN_API_BASEURL } from '../../common';
@@ -28,6 +29,20 @@ function templateFromSaved(saved: SavedObject<TemplateRaw>): Template {
   };
 }
 
+function handleClientError(err, response) {
+  const payload = err.output.payload;
+
+  // Determine if we can accurately report the error to the client
+  if (payload.statusCode === 404) {
+    return response.notFound({ body: payload });
+  } else {
+    // This will dump an error log to Kibana, and I'm not sure if that's ideal
+    return response.internalError({
+      body: err,
+    });
+  }
+}
+
 export function defineRoutes(router: IRouter /* , savedObjects: SavedObjectsServiceSetup*/) {
   router.get(
     {
@@ -45,10 +60,11 @@ export function defineRoutes(router: IRouter /* , savedObjects: SavedObjectsServ
 
   router.get(
     {
-      path: `${PLUGIN_API_BASEURL}/template/{id}`,
+      path: `${PLUGIN_API_BASEURL}/template`,
       validate: {
-        params: schema.object({
-          id: schema.maybe(schema.string()),
+        query: schema.object({
+          page: schema.maybe(schema.number({ min: 1 })),
+          perPage: schema.maybe(schema.number({ min: 1 })),
         }),
       },
       options: {
@@ -57,45 +73,64 @@ export function defineRoutes(router: IRouter /* , savedObjects: SavedObjectsServ
     },
     async (context: ScoreStackContext, request, response) => {
       const client = context.scorestack.getTemplatesClient();
-      const getTemplateById: boolean = request.params.id === undefined;
 
-      let savedObjects: Array<SavedObject<TemplateRaw>>;
+      // Set any optional values for the object find
+      const options: SavedObjectsFindOptions = {
+        type: 'template',
+      };
+      if (request.query.page !== undefined) {
+        options.page = request.query.page;
+      }
+      if (request.query.perPage !== undefined) {
+        options.perPage = request.query.perPage;
+      }
+
+      // Get the templates from the saved objects API
+      let resp: SavedObjectsFindResponse<TemplateRaw>;
       try {
-        if (getTemplateById) {
-          // If no ID was specified, just get all the templates
-          const resp: SavedObjectsFindResponse<TemplateRaw> = await client.find({
-            type: 'template',
-          });
-          savedObjects = resp.saved_objects;
-        } else {
-          // Only get the template of the specified ID
-          const resp: SavedObject<TemplateRaw> = await client.get('template', request.params.id);
-          savedObjects = [resp];
-        }
+        resp = await client.find(options);
       } catch (err) {
-        const payload = err.output.payload;
-
-        // Determine if we can accurately report the error to the client
-        if (payload.statusCode === 404) {
-          return response.notFound({ body: payload });
-        } else {
-          // This will dump an error log to Kibana, and I'm not sure if that's ideal
-          return response.internalError({
-            body: err,
-          });
-        }
+        return handleClientError(err, response);
       }
 
-      // If the client requested a template by ID, don't return it in an array
-      if (getTemplateById) {
-        return response.ok({
-          body: savedObjects.map((obj) => templateFromSaved(obj)),
-        });
-      } else {
-        return response.ok({
-          body: templateFromSaved(savedObjects[0]),
-        });
+      return response.ok({
+        body: {
+          total: resp.total,
+          page: resp.page,
+          per_page: resp.per_page,
+          templates: resp.saved_objects.map((obj) => templateFromSaved(obj)),
+        },
+      });
+    }
+  );
+
+  router.get(
+    {
+      path: `${PLUGIN_API_BASEURL}/template/{id}`,
+      validate: {
+        params: schema.object({
+          id: schema.string(),
+        }),
+      },
+      options: {
+        tags: ['access:template_management-read'],
+      },
+    },
+    async (context: ScoreStackContext, request, response) => {
+      const client = context.scorestack.getTemplatesClient();
+
+      // Get the template
+      let template: Template;
+      try {
+        const resp: SavedObject<TemplateRaw> = await client.get('template', request.params.id);
+        template = templateFromSaved(resp);
+      } catch (err) {
+        return handleClientError(err, response);
       }
+
+      return response.ok({
+        body: template,
+      });
     }
   );
 
