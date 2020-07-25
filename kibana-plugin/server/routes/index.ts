89f8330493981c45801f053ea80aadf313578a3b
Sean Newman <sxn6296@rit.edu>
2020-07-25 15:10:00 -0400

    Add DELETE ./template route

--- kibana-plugin/server/routes/index.ts
+++ kibana-plugin/server/routes/index.ts
@@ -84,20 +84,6 @@ async function createTemplateHelper(
 export function defineRoutes(router: IRouter /* , savedObjects: SavedObjectsServiceSetup*/) {
   router.get(
     {
-      path: `${PLUGIN_API_BASEURL}/example`,
-      validate: false,
-    },
-    async (context, request, response) => {
-      return response.ok({
-        body: {
-          time: new Date().toISOString(),
-        },
-      });
-    }
-  );
-
-  router.get(
-    {
       path: `${PLUGIN_API_BASEURL}/template`,
       validate: {
         query: schema.object({
@@ -210,6 +196,29 @@ export function defineRoutes(router: IRouter /* , savedObjects: SavedObjectsServ
           overwrite: true,
         }
       );
+    }
+  );
+
+  router.delete(
+    {
+      path: `${PLUGIN_API_BASEURL}/template/{id}`,
+      validate: {
+        params: idParamSchema,
+      },
+      options: {
+        tags: ['access:template_management-admin'],
+      },
+    },
+    async (context: ScoreStackContext, request, response) => {
+      const client = context.scorestack.getTemplatesClient();
+
+      try {
+        await client.delete('template', request.params.id);
+      } catch (err) {
+        return handleClientError(err, response);
+      }
+
+      return response.ok();
     }
   );
 }
