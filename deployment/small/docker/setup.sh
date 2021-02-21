8037eeb52c98992643b2a48c0bc794e46ad57b49
Sean Newman <sean@snewman.co>
2021-02-21 18:44:26 +0000

    Bump version

--- deployment/small/docker/setup.sh
+++ deployment/small/docker/setup.sh
@@ -65,7 +65,7 @@ EOF
 shred -uvz /tmp/cluster-passwords.txt
 
 # Install kibana plugin
-docker exec ${KIBANA_CONTAINER} /bin/bash -c "bin/kibana-plugin install https://github.com/scorestack/scorestack/releases/download/v0.6.2/kibana-plugin-v0.6.2.zip"
+docker exec ${KIBANA_CONTAINER} /bin/bash -c "bin/kibana-plugin install https://github.com/scorestack/scorestack/releases/download/v0.7.0-rc1/kibana-plugin-v0.7.0-rc1.zip"
 
 # Create admin user
 curl -k -XPOST -u elastic:${elastic_pass} ${ELASTICSEARCH_HOST}/_security/user/root -H "Content-Type: application/json" -d '{"password":"changeme","full_name":"Extra Superuser","email":"root@example.com","roles":["superuser"]}'
