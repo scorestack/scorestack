031c42bc466a4349cbf6a7272e084fb8199889b2
Sean Newman <sean@snewman.co>
2021-03-23 03:01:28 -0400

    Reorganize "beater" package

--- dynamicbeat/pkg/run/dynamicbeat.go
+++ dynamicbeat/pkg/run/dynamicbeat.go
@@ -1,4 +1,4 @@
-package beater
+package run
 
 import (
 	"crypto/tls"
@@ -9,9 +9,9 @@ import (
 	"sync"
 	"time"
 
-	"github.com/elastic/beats/v7/libbeat/beat"
-	"github.com/elastic/beats/v7/libbeat/common"
-	"github.com/elastic/beats/v7/libbeat/logp"
+	"github.com/elastic/beats/libbeat/beat"
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/logp"
 	elasticsearch "github.com/elastic/go-elasticsearch/v7"
 
 	"github.com/scorestack/scorestack/dynamicbeat/checks"
