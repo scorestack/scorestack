a70baf078d7439c241f5fc7a6342bf1e2554cb4f
Sean Newman <sean@snewman.co>
2021-03-23 04:16:06 -0400

    Get logging to stdout working properly

--- dynamicbeat/main.go
+++ dynamicbeat/main.go
@@ -15,8 +15,19 @@ limitations under the License.
 */
 package main
 
-import "github.com/scorestack/scorestack/dynamicbeat/cmd"
+import (
+	"log"
+
+	"github.com/scorestack/scorestack/dynamicbeat/cmd"
+	"go.uber.org/zap"
+)
 
 func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatalf("can't initialize zap logger: %v", err)
+	}
+	zap.ReplaceGlobals(logger)
+	defer logger.Sync()
 	cmd.Execute()
 }
