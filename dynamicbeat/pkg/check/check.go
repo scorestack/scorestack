8d4614da5a65b9a024958ab9aeef4b905d7858f9
Sean Newman <sean@snewman.co>
2021-03-23 23:41:53 -0400

    Re-add ValidationError

--- dynamicbeat/pkg/check/check.go
+++ dynamicbeat/pkg/check/check.go
@@ -1,6 +1,9 @@
 package check
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type Check interface {
 	GetConfig() Config
@@ -27,4 +30,15 @@ type Result struct {
 	Passed  bool
 	Message string
 	Details map[string]string
+}
+
+// A ValidationError represents an issue with a check definition.
+type ValidationError struct {
+	ID    string // the ID of the check with an invalid definition
+	Type  string // the type of the check with an invalid definition
+	Field string // the field in the check definition that was invalid
+}
+
+func (v ValidationError) Error() string {
+	return fmt.Sprintf("Error: check (Type: `%s`, ID: `%s`) is missing value for required field `%s`", v.Type, v.ID, v.Field)
 }
