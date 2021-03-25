0366131cf14b57710c9141c28d0cc8e250210d45
Sean Newman <sean@snewman.co>
2021-03-25 03:40:17 -0400

    Return status codes from req helper funcs

--- dynamicbeat/pkg/setup/client.go
+++ dynamicbeat/pkg/setup/client.go
@@ -18,13 +18,13 @@ type Client struct {
 	Kibana        string
 }
 
-func (c *Client) ReqElasticsearch(method string, path string, body io.Reader) (io.ReadCloser, error) {
+func (c *Client) ReqElasticsearch(method string, path string, body io.Reader) (int, io.ReadCloser, error) {
 	url := fmt.Sprintf("%s%s", c.Elasticsearch, path)
 
 	// Build request
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to build Elasticsearch request to '%s': %s", path, err)
+		return 0, nil, fmt.Errorf("failed to build Elasticsearch request to '%s': %s", path, err)
 	}
 	req.SetBasicAuth(c.Username, c.Password)
 	req.Header.Set("kbn-xsrf", "true")
@@ -35,18 +35,18 @@ func (c *Client) ReqElasticsearch(method string, path string, body io.Reader) (i
 	// Send request
 	res, err := c.Inner.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send Elasticsearch request to '%s': %s", path, err)
+		return 0, nil, fmt.Errorf("failed to send Elasticsearch request to '%s': %s", path, err)
 	}
-	return res.Body, nil
+	return res.StatusCode, res.Body, nil
 }
 
-func (c *Client) ReqKibana(method string, path string, body io.Reader) (io.ReadCloser, error) {
+func (c *Client) ReqKibana(method string, path string, body io.Reader) (int, io.ReadCloser, error) {
 	url := fmt.Sprintf("%s%s", c.Kibana, path)
 
 	// Build request
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to build Kibana request to '%s': %s", path, err)
+		return 0, nil, fmt.Errorf("failed to build Kibana request to '%s': %s", path, err)
 	}
 	req.SetBasicAuth(c.Username, c.Password)
 	req.Header.Set("kbn-xsrf", "true")
@@ -57,9 +57,9 @@ func (c *Client) ReqKibana(method string, path string, body io.Reader) (io.ReadC
 	// Send request
 	res, err := c.Inner.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send Kibana request to '%s': %s", path, err)
+		return 0, nil, fmt.Errorf("failed to send Kibana request to '%s': %s", path, err)
 	}
-	return res.Body, nil
+	return res.StatusCode, res.Body, nil
 }
 
 type clusterHealth struct {
@@ -76,7 +76,7 @@ type kibanaStatus struct {
 
 func (c *Client) Wait() error {
 	for {
-		body, err := c.ReqElasticsearch("GET", "/_cluster/health", nil)
+		_, body, err := c.ReqElasticsearch("GET", "/_cluster/health", nil)
 		if err != nil {
 			return err
 		}
@@ -95,7 +95,7 @@ func (c *Client) Wait() error {
 	}
 
 	for {
-		body, err := c.ReqKibana("GET", "/api/status", nil)
+		_, body, err := c.ReqKibana("GET", "/api/status", nil)
 		if err != nil {
 			return err
 		}
