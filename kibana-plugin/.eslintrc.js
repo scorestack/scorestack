b20fd43b38742b1eeff4af9c4acdb70fd567f087
Sean Newman <sean@snewman.co>
2022-04-08 20:17:04 -0400

    Update associated files from plugin gen. v8.1.2

--- kibana-plugin/.eslintrc.js
+++ kibana-plugin/.eslintrc.js
@@ -3,7 +3,5 @@ module.exports = {
   extends: ['@elastic/eslint-config-kibana', 'plugin:@elastic/eui/recommended'],
   rules: {
     '@kbn/eslint/require-license-header': 'off',
-    '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/class-name-casing': 'off',
   },
 };
