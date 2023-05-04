afc49d698580622e8520b50e351074a7a82c460c
Jacob Highfield <camperboy1000@gmail.com>
2023-05-04 15:58:13 -0400

    Bump golangci-lint to 1.52.2

--- dockerfiles/scripts/dynamicbeat/lint.sh
+++ dockerfiles/scripts/dynamicbeat/lint.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 export PATH="$PATH:$GOPATH/bin"
 cd $HOME/scorestack/dynamicbeat
 make
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.25.1
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.52.2
 golangci-lint run -v
